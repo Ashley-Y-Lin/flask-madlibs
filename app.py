e95dbb44ca676f81898da0e5a9161d4ed776bc0c
Timothy Sukamtoh <timtoh168@gmail.com>
2023-04-19 16:14:54 -0700

    making questions page

--- app.py
+++ app.py
@@ -7,3 +7,9 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
+
+@app.get("/")
+def homepage():
+    """Loads the homepage"""
+
+    return render_template("questions.html", prompts=silly_story.prompts)
